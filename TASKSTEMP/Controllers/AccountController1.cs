eb4abbd1dac4b121df9cfd804eec84e34d1075b0
ahmedob21 <ahmedob22@gmail.com>
2024-04-26 17:49:50 +0300

    Add project files.

---
+++ TASKSTEMP/Controllers/AccountController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TASKSTEMP.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ TASKSTEMP/Controllers/adminController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TASKSTEMP.Controllers
+{
+    public class adminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Analytics()
+        {
+            return View();
+        }
+
+        public IActionResult Chat() 
+        {
+            return View();
+        }
+
+        public IActionResult Contact() 
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult Email_Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult Email_Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Email_Compose()
+        {
+            return View();
+        }
+
+        public IActionResult Alert()
+        {
+            return View();
+        }
+    }
+}
